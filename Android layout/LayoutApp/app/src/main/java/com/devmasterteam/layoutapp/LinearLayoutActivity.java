605ff175334b9a09499e42f8730dd1b5d6dde5d8
DevMasterTeam <devmasterteam@gmail.com>
2019-09-12 14:26:49 -0300

    Adicionando projeto criado para aula de LinearLayout.

---
+++ Android layout/LayoutApp/app/src/main/java/com/devmasterteam/layoutapp/LinearLayoutActivity.java
@@ -0,0 +1,14 @@
+package com.devmasterteam.layoutapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LinearLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout);
+    }
+}
